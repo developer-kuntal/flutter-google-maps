ec4ed1f71e2acde5f4673fafcd752c7c451b1c7a
developer-kuntal <kuntal.iiest@gmail.com>
2019-04-13 23:07:12 +0530

    Flutter Geolocation App update at 1.07 PM 4/13/2019

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,10 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+
 
 void main() {
   runApp(new MaterialApp(
@@ -13,10 +19,77 @@ class GoogleMaps extends StatefulWidget {
 }
 
 class _GoogleMapsState extends State<GoogleMaps> {
+
+  bool mapToggle = false;
+  var currentLocation;
+  var clients = [];
+  GoogleMapController mapController;
+  Completer<GoogleMapController> _controller = Completer();
+  
+  static final CameraPosition _kLake = CameraPosition(
+      bearing: 192.8334901395799,
+      target: LatLng(37.43296265331129, -122.08832357078792),
+      tilt: 59.440717697143555,
+      zoom: 19.151926040649414);
+
+  // @override
+  void initState() {
+    super.initState();
+    Geolocator().getCurrentPosition().then((currloc) {
+      setState(() {
+       currentLocation = currloc;
+       mapToggle = true; 
+       populateClients();
+      });
+    });
+  }
+
+  populateClients() {
+    clients = [];
+    // Firestore.instance.
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      appBar: new AppBar(
+        title: Text("Google Maps Demo"),
+      ),
+      body: Column(
+        children: <Widget>[
+          Stack(
+            children: <Widget>[
+              Container(
+                
+                height: MediaQuery.of(context).size.height - 100.0,
+                width: double.infinity,
+                child: mapToggle ? GoogleMap(
+                        mapType: MapType.hybrid,
+                        onMapCreated:  (GoogleMapController controller) {
+                          _controller.complete(controller);
+                        },
+                        initialCameraPosition: CameraPosition(
+                          target: LatLng(currentLocation.latitude, currentLocation.logitude),
+                          zoom: 10.0
+                        ),
+                      ) : Center(child: 
+                      Text('Loading...Please wait', style: TextStyle(
+                        fontSize: 20.0
+                      ),),)
+              )
+            ],
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: _goToTheLake,
+        label: Text('To the lake!'),
+        icon: Icon(Icons.directions_boat),
+      ),
     );
+  }
+  Future<void> _goToTheLake() async {
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
   }
 }
